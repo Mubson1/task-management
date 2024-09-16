1aaba0c3cba8735a5abd0776d1ff66ac0a14169c
mubson11 <mubson.karki+gitlab@innovatetech.co>
2024-09-16 20:43:48 +0545

    updates[defaultsetting bookassets]: api response change, table header change, show total value in table of book assets, default setting minor issue fix

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,20 @@
-const OrganizationIdPage = () => {
-  return <div>Organization</div>;
+import { db } from "@/lib/db";
+import { Form } from "./form";
+import { Board } from "./board";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      <div className="space-y-2">
+        {boards.map((board) => (
+          <Board key={board.id} id={board.id} title={board.title} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
